095180c9b388624b82cc5df6363d2ee45576ff36
zyt2298122 <1045524510@qq.com>
2018-10-11 16:02:28 +0800

    vue

--- src/main/java/com/example/demo/controller/AjaxController.java
+++ src/main/java/com/example/demo/controller/AjaxController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +16,16 @@ public class AjaxController {
     @RequestMapping("/ajax1")
     public Object ajax1(){
         String msg = "请求成功！";
+        Map map = new HashMap();
+        map.put("msg",msg);
+        return map;
+    }
+
+    @RequestMapping("/ajax2")
+    public Object ajax2(HttpServletRequest request){
+        String a = request.getParameter("a");
+        String b = request.getParameter("b");
+        String msg = "请求成功！传入参数为：a:"+a+" b:"+b;
         Map map = new HashMap();
         map.put("msg",msg);
         return map;
